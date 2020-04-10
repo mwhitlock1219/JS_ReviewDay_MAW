2107dcbb99501d6e0faae28bd6a0f48344fa70b0
Melina Alejos <malejos1219@gmail.com>
2020-04-09 20:38:40 -0400

    modified file added file

--- pizza.js
+++ pizza.js
@@ -12,3 +12,27 @@ function description (toppings){
 //Log function to console then Call function and pass array through function
 
 console.log(description(toppings));
+
+
+// //***** Another Method to Answer Question *****
+
+// //Created a function to return array as a string.
+
+// function description (){
+// 	var addtoppings = toppings.join(' and ');
+// 	return addtoppings;
+// }
+// //logs to array to console
+
+// console.log(description());
+
+
+// // Modifying the function above to retunr string and another string.
+
+// function description (){
+// 	var addtoppings = toppings.join(' and ');
+// 	return 'A delicious pizza that has ' + addtoppings;
+// }
+// //logs to array to console with both strings
+
+// console.log(description());
